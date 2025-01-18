3d8dc1fc5d5f3f0f3cada97c8a3d79033904364f
d33p0st <d33p0st@gmail.com>
2025-01-19 04:35:21 +0530

    Update: CSS Fix for darkmode

---
+++ assets/js/homepage-dark-mode-specific.js
@@ -0,0 +1,19 @@
+function homepageSpecific(str) {
+    if (typeof str !== "string") {
+        throw new TypeError("str param must be a string.");
+    }
+
+    const nameImageElement = document.getElementById('homepage-name-pic');
+
+    if (str === "dark") {
+        if (nameImageElement !== null) {
+            nameImageElement.src = '/assets/images/name-dark.gif';
+        }
+    }  else {
+        if (nameImageElement !== null) {
+            nameImageElement.src = '/assets/images/name-light.gif';
+        }
+    }
+}
+
+// export default homepageSpecific;
